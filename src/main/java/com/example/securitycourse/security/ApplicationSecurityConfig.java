bdedc58071475f27ac9c6356bfb2cf853eb7824f
Josip-github <josipcota1@gmail.com>
2021-08-03 16:05:53 +0200

    Rememeber me cookie and extra options

--- src/main/java/com/example/securitycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securitycourse/security/ApplicationSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -43,7 +45,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe(); // defaults to 2 weeks
+                .rememberMe() // defaults to 2 weeks
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured");
     }
 
     @Override
